9ad17d3e22f65e149db33685cd92fdbdaa88a766
Yuri Salmiyanov <func_breakable@yahoo.com>
2016-05-06 20:29:00 +0300

    first step

--- comments.js
+++ comments.js
@@ -1,1 +1,5 @@
-alert("Hello!");
+function clicked(){
+  var commentsElement = document.createElement('tr');
+  commentsElement.innerHTML = '<td><p>'+document.getElementById('TextOfComment').value+'</p><p align=right><button>Ответить</button></p></td>';
+  document.getElementById('TableOfComments').appendChild(commentsElement);
+}
